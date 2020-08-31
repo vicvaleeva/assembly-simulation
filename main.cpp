bfbe39f067ec55e386ffe4ad1582be25d9f056ff
Victoria Emerson <valeevtim@gmail.com>
2020-08-31 15:03:52 +0300

    finish main interface

--- main.cpp
+++ main.cpp
@@ -8,11 +8,40 @@ using namespace std;
 int n_o, a_o, dt_o, finished_o, n_n, a_n, dt_n, finished_n;
 Old oldsys;
 New newsys;
+int hour = 0, minute = 0, second = 0;
 
 void delay(int ms) {
 	ms *= 1000;
 	clock_t timeDelay = ms + clock();
 	while(clock() < timeDelay);
+}
+
+void printData() {
+	system("printf \"\033c\"");
+	cout << "\n\n\n" << "        " << oldsys.getFinished() << "          " << newsys.getFinished() << "          ";
+	cout << hour << " : " << minute << " : " << second << "\n\n\n";
+}
+
+void cycle() {
+	while(true) {
+		int current_time = hour * 3600 + minute * 60 + second;
+		oldsys.update(current_time);
+		newsys.update(current_time);
+
+		printData();
+		++second;
+		
+		if (second > 59) {
+			++minute;
+			second = 0;
+		}	
+
+		if (minute > 59) {
+			++hour;
+			minute = 0;
+		}
+		
+	}
 }
 
 int main() {
