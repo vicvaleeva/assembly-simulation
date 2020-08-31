9b231b9ee1f8e6b6f12104396683056574e36139
Victoria Emerson <valeevtim@gmail.com>
2020-08-31 19:04:35 +0300

    finish w/out probability

--- main.cpp
+++ main.cpp
@@ -9,26 +9,40 @@ int n_o, a_o, dt_o, finished_o, n_n, a_n, dt_n, finished_n;
 Old oldsys;
 New newsys;
 int hour = 0, minute = 0, second = 0;
+int stoptimeOld, stoptimeNew;
+bool isOldStopped, isNewStopped;
 
 void delay(int ms) {
-	ms *= 1000;
+	ms *= 20;
 	clock_t timeDelay = ms + clock();
 	while(clock() < timeDelay);
 }
 
 void printData() {
 	system("printf \"\033c\"");
-	cout << "\n\n\n" << "        " << oldsys.getFinished() << "          " << newsys.getFinished() << "          ";
+	cout << "\n\n";
+	cout << "   OLD: N = " << n_o << ", a = " << a_o << ", t = " << dt_o << ", finished " << finished_o << "\n";
+	cout << "   NEW: N = " << n_n << ", a = " << a_n << ", t = " << dt_n << ", finished " << finished_n << "\n";
+	cout << "\n\n\n" << "        old" << "          new" << "             time\n\n";
+	cout << "        " << oldsys.getFinished() << "          " << newsys.getFinished() << "          ";
 	cout << hour << " : " << minute << " : " << second << "\n\n\n";
+	if (oldsys.getFinished() > 0) {
+		double nw = (newsys.getFinished() - finished_n) * 1.0;
+		double ol = (oldsys.getFinished() - finished_o) * 1.0;
+		double ratio = nw / ol;
+		cout << "   Current ratio (old / new): " << ratio << "\n";
+	}
+	cout << "\n   Press Ctrl+Z to exit\n";
 }
 
 void cycle() {
 	while(true) {
 		int current_time = hour * 3600 + minute * 60 + second;
-		oldsys.update(current_time);
+		oldsys.update(current_time);	
 		newsys.update(current_time);
 
 		printData();
+		delay(1000);
 		++second;
 		
 		if (second > 59) {
@@ -76,7 +90,6 @@ int main() {
 	cout << "Number of products finished: ";
 	cin >> finished_n;
 	cout << "\n\n";
-
 	oldsys.init(n_o, a_o, dt_o, finished_o);
 	newsys.init(n_n, a_n, dt_n, finished_n);
 
--- new/new.cpp
+++ new/new.cpp
@@ -8,7 +8,7 @@ void New::init(int set_n, int set_a, int set_dt, int set_finished) {
 }
 
 void New::update(int time) {
-	if (time % (a + 2 * dt) == 0) {
+	if (time % (a + 2 * dt) == 0 && time != 0) {
 		finished += n;
 	}
 }
